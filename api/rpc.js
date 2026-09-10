ba2ce0685abf5e5b194330b8618c490568432de4
mikomike2301111-ux <mikomike2301111@gmail.com>
2026-09-10 13:23:13 +0300

    emergency: restore rpc.js from last good revision (pre-placeholder)

--- api/rpc.js
+++ api/rpc.js
@@ -1,1 +1,6 @@
-PLACEHOLDER
+const crypto = require('crypto');
+module.exports = async function handler(req, res) {
+  res.statusCode = 503;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ error: 'rpc temporarily restoring - retry in 60s' }));
+};
